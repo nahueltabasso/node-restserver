86d12d8af0d974d29abf40a739a34de7a155b0d3
NahuelRc10 <nahueltabasso@gmail.com>
2020-10-21 13:58:06 -0300

    First commit

---
+++ server/server.js
@@ -0,0 +1,42 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+ 
+app.get('/usuario', function (req, res) {
+  res.json('get Usuario');
+});
+
+app.post('/usuario', function (req, res) {
+    let body = req.body; 
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario!',
+        });
+    } else {
+        res.json({
+            body
+        });
+    }
+});
+
+app.delete('/usuario', function (req, res) {
+    res.json('delete Usuario');
+});
+
+app.put('/usuario/:id', function (req, res) {
+    let id = req.params.id;
+    res.json({
+        id: id
+    });
+});
+ 
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando puerto: ", process.env.PORT);
+});
