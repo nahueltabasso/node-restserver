31a64e9ef8c9ea474a4bfb1806f0af359c7edb71
NahuelRc10 <nahueltabasso@gmail.com>
2020-10-26 16:59:08 -0300

    Variable de entorno mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://restservernodejs:wNwyPtW11yjNJnF0@cluster0.2jfde.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB
